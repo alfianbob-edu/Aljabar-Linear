c23db1bba1c8882a59a2f6509211eb37d00e6b5a
alfianbob-edu <ahardiansyah.edu@gmail.com>
2021-10-31 00:15:42 +0700

    Update directory

---
+++ Section 2/MatriksOperation/MatriksPythonOperation001.py
@@ -0,0 +1,21 @@
+#matriks addition
+from numpy import array
+
+#define 1st matriks
+A = array ([
+    [1,2,3],
+    [4,5,6]
+])
+
+#define 2nd matriks
+B = array([
+    [1,2,3],
+    [4,5,6]
+])
+
+#result
+C = A+B
+
+print(A)
+print(B)
+print(C)
